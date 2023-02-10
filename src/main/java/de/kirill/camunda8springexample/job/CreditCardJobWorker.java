3e7ec9ceaf05eea306d81e40d123c4f25ea2d3ac
kirill.esau <kirill.esau@adesso.de>
2023-02-10 11:04:03 +0100

    added logging

--- src/main/java/de/kirill/camunda8springexample/job/CreditCardJobWorker.java
+++ src/main/java/de/kirill/camunda8springexample/job/CreditCardJobWorker.java
@@ -5,17 +5,20 @@ import io.camunda.zeebe.client.api.response.ActivatedJob;
 import io.camunda.zeebe.spring.client.annotation.JobWorker;
 import io.camunda.zeebe.spring.client.annotation.VariablesAsType;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 @RequiredArgsConstructor
 @Component
+@Slf4j
 public class CreditCardJobWorker {
 
     private final CreditCardService creditCardService;
 
     @JobWorker(type = "chargeCreditCard")
-    public void handleChargeCreditCard(final ActivatedJob job, @VariablesAsType CreditCardVariables creditCardDTO) {
-        creditCardService.chargeCreditCard();
+    public void handleChargeCreditCard(final ActivatedJob job, @VariablesAsType CreditCardVariables creditCardVariables) {
+        log.trace("start Job for Bpmn-id: " + job.getBpmnProcessId());
+        creditCardService.chargeCreditCard(creditCardVariables);
     }
 
 }
--- src/main/java/de/kirill/camunda8springexample/service/CreditCardService.java
+++ src/main/java/de/kirill/camunda8springexample/service/CreditCardService.java
@@ -1,12 +1,21 @@
 package de.kirill.camunda8springexample.service;
 
+import de.kirill.camunda8springexample.job.CreditCardVariables;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class CreditCardService {
 
-    public void chargeCreditCard() {
-        System.out.println("Charging Credit Card");
+    public void chargeCreditCard(CreditCardVariables creditCardVariables) {
+        log.trace("Charging Credit Card: " + creditCardVariables.cardNumber());
+
+        log.info("Credit Card Data: ");
+        log.info("- reference: " + creditCardVariables.reference());
+        log.info("- amount: " + creditCardVariables.amount());
+        log.info("- cardExpiry: " + creditCardVariables.cardExpiry());
+        log.info("- cardCVC: " + creditCardVariables.cardCVC());
     }
 
 }
