7bf9ec0164dfcb49bbf17b9b8949ca055384891b
kirill.esau <kirill.esau@adesso.de>
2023-02-10 11:04:58 +0100

    test fix

--- src/test/java/de/kirill/camunda8springexample/Camunda8SpringExampleApplicationTests.java
+++ src/test/java/de/kirill/camunda8springexample/Camunda8SpringExampleApplicationTests.java
@@ -1,9 +1,11 @@
 package de.kirill.camunda8springexample;
 
+import io.camunda.zeebe.spring.test.ZeebeSpringTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@ZeebeSpringTest
 class Camunda8SpringExampleApplicationTests {
 
     @Test
